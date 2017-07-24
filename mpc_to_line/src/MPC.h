232430eede5357830b19dddd219532c10d072710
dabbl <dabbl@home.de>
2017-07-24 21:31:11 +0200

    minor reformatting, inserted code from solution

--- mpc_to_line/src/MPC.h
+++ mpc_to_line/src/MPC.h
@@ -6,16 +6,17 @@
 
 using namespace std;
 
-class MPC {
- public:
-  MPC();
+class MPC 
+{
+public:
+    MPC();
 
-  virtual ~MPC();
+    virtual ~MPC();
 
-  // Solve the model given an initial state.
-  // Return the next state and actuations as a
-  // vector.
-  vector<double> Solve(Eigen::VectorXd x0, Eigen::VectorXd coeffs);
+    // Solve the model given an initial state.
+    // Return the next state and actuations as a
+    // vector.
+    vector<double> Solve(Eigen::VectorXd x0, Eigen::VectorXd coeffs);
 };
 
 #endif /* MPC_H */
